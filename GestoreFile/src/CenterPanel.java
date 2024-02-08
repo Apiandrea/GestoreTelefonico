c9ac394ee8dba553c8a1b73d1154d54485cca250
Apiandrea <apinadrea@gmail.com>
2024-02-08 19:59:55 +0100

    Aggiunta la shadow per ogni field.

--- GestoreFile/src/CenterPanel.java
+++ GestoreFile/src/CenterPanel.java
@@ -1,16 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class CenterPanel extends JPanel {
 
-    Font font1 = new Font(Font.MONOSPACED, Font.PLAIN, 20);
+    Font font1 = new Font(Font.SERIF, Font.PLAIN, 20);
     JLabel labelName = new JLabel("Nome: ");
     JLabel labelSurname = new JLabel("Cognome: ");
     JLabel labelTel = new JLabel("Num. Tel.: ");
 
-    JTextField fieldName = new JTextField(20);
-    JTextField fieldTel = new JTextField(20);
-    JTextField fieldSurname = new JTextField(20);
+    JTextField fieldName = new JTextField("Inserisci...", 20);
+    JTextField fieldTel = new JTextField("Inserisci...",20);
+    JTextField fieldSurname = new JTextField("Inserisci...",20);
 
     public CenterPanel()
     {
@@ -22,14 +24,14 @@ public class CenterPanel extends JPanel {
         JPanel panelSouth = new JPanel(new FlowLayout());
 
         panelNorth.add(initializeLabel(labelName));
-        panelNorth.add(fieldName);
+        panelNorth.add(initializeTextField(fieldName));
         panelNorth.setBackground(Color.gray);
         panelCenter.add(initializeLabel(labelSurname));
         panelCenter.setBackground(Color.gray);
-        panelCenter.add(fieldSurname);
+        panelCenter.add(initializeTextField(fieldSurname));
         panelSouth.add(initializeLabel(labelTel));
         panelSouth.setBackground(Color.gray);
-        panelSouth.add(fieldTel);
+        panelSouth.add(initializeTextField(fieldTel));
 
         add(panelNorth);
         add(panelCenter);
@@ -45,8 +47,31 @@ public class CenterPanel extends JPanel {
 
     public JTextField initializeTextField(JTextField t)
     {
-        t.setPreferredSize(new Dimension(150, 5));
+        t.setPreferredSize(new Dimension(150, 25));
+        t.setName("Inserisci...");
+        applyShadow(t);
 
         return t;
+    }
+
+    public void applyShadow(JTextField f)
+    {
+        f.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(f.getText().equals(f.getName())) {
+                    f.setText("");
+                    f.setForeground(Color.black);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(f.getText().isEmpty()) {
+                    f.setText(f.getName());
+                    f.setForeground(Color.gray);
+                }
+            }
+        });
     }
 }
